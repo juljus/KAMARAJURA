424a19b0af81d0dc107fbcb410e7b06dc1dd7f9a
Juljus <juljus@gmail.com>
2025-01-30 14:43:04 +0200

    added default layout

---
+++ nuxt/components/navbar.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <nav class="p-4 mb-8 border-b-2 border-gray-400">
+            <ul class="flex space-x-4">
+                <li class="p-4"> <NuxtLink to="/">KAMARAJURA</NuxtLink> </li>
+                <li class="p-4">SEARCH</li>
+                <li class="p-4">LOGIN</li>
+            </ul>
+        </nav>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+</style>
---
+++ nuxt/layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="text-gray-400 bg-stone-900 font-mono h-screen">
+        <header>
+
+            <!-- Navbar -->
+            <navbar/>
+
+        </header>
+
+        <!-- Page content -->
+        <div class="page-content">
+            <slot/>
+        </div>
+
+    </div>
+</template>
+
+<style scoped>
+
+</style>
