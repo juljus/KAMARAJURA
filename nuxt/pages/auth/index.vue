35982b87a56b416d0af792ad7e90bcbe0f89a79e
KittMi <148247392+KittMi@users.noreply.github.com>
2025-04-29 16:41:18 +0300

    uuesti keskele

--- nuxt/pages/auth/index.vue
+++ nuxt/pages/auth/index.vue
@@ -8,7 +8,7 @@
             <label for="Password" class="block mb-2 text-gray-400 bg-stone-900 font-mono">Password</label>
             <input type="Password" id="user_password" class="bg-stone-950 border-b-2 border-gray-400 text-gray-400 font-mono rounded-lg focus:ring-blue-900 focus:border-blue-900 block w-full p-2.5" placeholder="Pass HERE" required />"
         </div>
-        <button type="submit" class="text-gray-400 bg-sky-900 hover:bg-sky-950 focus:ring-blue-900 rounded-lg font-mono border-b-2 w-full sm:w-auto px-5 py-2.5 text-center content-center">Submit :P</button>
+        <button type="submit" class="text-gray-400 bg-sky-900 hover:bg-sky-950 focus:ring-blue-900 rounded-lg font-mono border-b-2 w-full sm:w-auto px-5 py-2.5 text-center justify-center">Submit :P</button>
     </div>
 </template>
 
