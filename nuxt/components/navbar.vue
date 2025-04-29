64088ee65c05623c580fa5b92aed916e40d85bc2
KittMi <148247392+KittMi@users.noreply.github.com>
2025-04-29 17:40:30 +0300

    navbar responsive ja login ilusamaks

--- nuxt/components/navbar.vue
+++ nuxt/components/navbar.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
-        <nav class="p-4 mb-8 border-b-2 border-gray-400">
+        <nav class="p-10 mb-8 border-b-2 border-gray-400">
             <ul class="grid-cols-3 grid">
-                <li class="p-4 text-center"> <NuxtLink to="/">KAMARAJURA</NuxtLink> </li>
-                <li class="p-4 text-center"> <NuxtLink to="/search/">FIND GAMES</NuxtLink></li>
-                <li class="p-4 text-center"> <NuxtLink to="/auth/">LOGIN</NuxtLink> </li>
+                <li class="p-4 text-center transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:text-gray-300"> <NuxtLink to="/">KAMARAJURA</NuxtLink> </li>
+                <li class="p-4 text-center transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:text-gray-300"> <NuxtLink to="/search/">FIND GAMES</NuxtLink></li>
+                <li class="p-4 text-center transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:text-gray-300"> <NuxtLink to="/auth/">LOGIN</NuxtLink> </li>
             </ul>
         </nav>
     </div>
