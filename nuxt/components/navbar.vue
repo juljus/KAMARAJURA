2cb278f247bac01cb3f13a642bc2f4ceee8cb073
KittMi <148247392+KittMi@users.noreply.github.com>
2025-04-29 15:35:39 +0300

    navbar search

--- nuxt/components/navbar.vue
+++ nuxt/components/navbar.vue
@@ -3,7 +3,7 @@
         <nav class="p-4 mb-8 border-b-2 border-gray-400">
             <ul class="grid-cols-3 grid">
                 <li class="p-4 text-center"> <NuxtLink to="/">KAMARAJURA</NuxtLink> </li>
-                <li class="p-4 text-center">SEARCH</li>
+                <li class="p-4 text-center"> <NuxtLink to="/search/">FIND GAMES</NuxtLink></li>
                 <li class="p-4 text-center"> <NuxtLink to="/auth/">LOGIN</NuxtLink> </li>
             </ul>
         </nav>
